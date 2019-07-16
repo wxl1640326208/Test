9134ddce4b72208c800030513cbeed09a824592a
wangxulong <1640326208@qq.com>
2019-07-16 10:59:11 +0800

    add2 Test

--- EasyUITreeNode.java
+++ EasyUITreeNode.java
@@ -7,35 +7,4 @@ public class EasyUITreeNode implements Serializable {
     private String text;
     private String state;
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-	 public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-	
-}
+ }
